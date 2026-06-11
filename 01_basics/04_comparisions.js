cac42f3b6a85445f944cb8fbc2c3fada5ea20ac3
ananyagarg11 <gargananya8789@gmail.com>
2026-06-11 15:01:09 +0000

    conversions and comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,13 @@
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (checks data type also)
+console.log(2==="2")
