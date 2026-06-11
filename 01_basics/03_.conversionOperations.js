3e3e71c665f1793b8ac16eaa1ee00a1969b9bf0b
ananyagarg11 <gargananya8789@gmail.com>
2026-06-11 13:11:05 +0000

    conversion of data types

---
+++ 01_basics/03_.conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = "ananya"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" = 33
+//"33abs" = NaN
+// true = 1 false = 0 
+
+let isLoggedIn = "ananya"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 =>true 2=> false
+// ""= false "ananya"= true 
+
+let someNumber = 33
+let stringNumber = String(someNumber )
+console.log(typeof stringNumber)
