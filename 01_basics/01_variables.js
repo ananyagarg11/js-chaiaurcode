eab9d20be262dccdc31767db07504a7c4f395c9b
ananyagarg11 <gargananya8789@gmail.com>
2026-06-11 10:12:07 +0000

    data types and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "agarg11_123@gmail.com"
+var accPassowrd = "12345"
+accountCity = "Jaipur"
+let AccountState;
+
+
+// accountId = 2
+accountEmail = "shikharsharma123@gmail.com"
+accPassword = "3456"
+accountCity = "banglore"
+console.log(accountId)
+
+/*
+dont use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId, accountEmail, accPassword, accountCity, AccountState])
